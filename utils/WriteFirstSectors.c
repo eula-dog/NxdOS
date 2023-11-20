51b4a63d777c9742b5cb1b7dbb03597da5659427
callmeatsep <1768451152@qq.com>
2023-11-20 11:19:46 +0800

    第三次修改

--- utils/WriteFirstSectors.c
+++ utils/WriteFirstSectors.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>   //lib头文件用于调用system函数
 
-int main() {
-    FILE *file;
-    char buffer[100];
 
-    // 打开文件
-    file = fopen("D:\\NxdOS\\NxdOS\\utils\\boot.bin", "r");
-    if (file == NULL) {
-        printf("无法打开文件\n");
+
+void printBinary(unsigned char byte) {
+    for (int i = 7; i >= 0; i--) {
+        if ((byte >> i) & 1) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+    }
+    printf(" ");
+}
+
+//"D:\\NxdOS\\NxdOS\\utils\\boot.bin"
+//D:\\NxdOS\\NxdOS\\utils\\result\\result.img
+int main() {
+
+    char write_path[] = "D:\\NxdOS\\NxdOS\\utils\\result\\result.img";
+
+    FILE *input_file, *output_file;
+    unsigned char buffer[1024];
+    size_t bytes_read;
+
+    // 打开输入二进制文件
+    input_file = fopen("D:\\NxdOS\\NxdOS\\utils\\boot.bin", "rb");
+    if (input_file == NULL) {
+        printf("无法打开输入文件\n");
         return 1;
     }
 
-    // 读取文件内容
-    while (fgets(buffer, 100, file) != NULL) {
-        printf("%s", buffer);
+    // 创建或打开输出二进制文件
+    output_file = fopen(write_path, "wb");
+    if (output_file == NULL) {
+        printf("无法打开输出文件\n");
+        fclose(input_file);
+        return 1;
+    }
+
+    // 从输入文件读取数据并写入输出文件
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), input_file)) > 0) {
+        fwrite(buffer, 1, bytes_read, output_file);
     }
 
     // 关闭文件
-    fclose(file);
+    fclose(input_file);
+    fclose(output_file);
 
     system("pause");
 
